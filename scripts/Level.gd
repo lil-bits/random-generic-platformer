e7861d2911c082287026c0880acc545d6f8bd605
Ondrej Slinták <ondrowan@gmail.com>
2018-10-20 10:07:21 +0200

    Assert if level has HUD
    Instead of running some code only if Level contains HUD, I've added an
    assert indicating that HUD is needed for some functionality.

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -3,5 +3,6 @@ extends Node
 export(String) var level_name
 
 func _ready():
-	if has_node("HUD"):
-		$HUD.show_start_level_message(level_name)
+	assert(has_node("HUD"))
+
+	$HUD.show_start_level_message(level_name)
