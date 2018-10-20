40cd91073a8c90ac5f485b52f37d68340d9d7a58
Ondrej Slinták <ondrowan@gmail.com>
2018-10-20 21:35:19 +0200

    Add coins to HUD

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+var coins = 0
+
+func _ready():
+    get_node("/root/Level").connect("update_hud", self, "update_values")
+
+    update_values({"coins": coins})
+
 func show_message(message):
     $Message.text = message
     $Message.show()
@@ -8,3 +15,6 @@ func show_start_level_message(message):
     show_message(message)
     yield(get_tree().create_timer(2.0), "timeout")
     $Message.hide()
+
+func update_values(values):
+    $Container/Label.text = "x{coins}".format({"coins": values.coins})
