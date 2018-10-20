babcb7452e904a6d10f99e85e64c89eac72055db
Yanchi Toth <yanchith@users.noreply.github.com>
2018-10-20 18:19:28 +1000

    Improve jumping controls
    Previously, if the player held the jump button, the character would jump again
    as soon as it collided with a platform. Now releasing the jump key is required before
    jumping again, making the controls feel more like what people are used to.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,27 +7,27 @@ const ACCELERATION = 50
 const JUMP_HEIGHT_LOW = -420
 const JUMP_HEIGHT_HIGH = -550
 
-
 var motion = Vector2()
-var score = 0
-var health = 5
+
 
 func _ready():
 	pass
-	
+
+
 func _process(delta):
 	pass
 
+
 func _physics_process(delta):
 	motion.y += GRAVITY
-	
-	var go_up_high = Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up")
-	var go_up_low = Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down")
-	var go_left = Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left")
-	var go_right = Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right")
+
+	var go_left = Input.is_action_pressed("ui_left")
+	var go_right = Input.is_action_pressed("ui_right")
+	var jump_low = Input.is_action_just_pressed("ui_down")
+	var jump_high = Input.is_action_just_pressed("ui_up")
 	var is_idle = not go_left and not go_right
 	var apply_friction = is_idle
-	
+
 	if go_right:
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$AnimatedSprite.flip_h = false
@@ -38,11 +38,11 @@ func _physics_process(delta):
 		$AnimatedSprite.play("Run")
 	elif is_idle:
 		$AnimatedSprite.play("Idle")
-		
+
 	if is_on_floor():
-		if go_up_high:
+		if jump_high:
 			motion.y = JUMP_HEIGHT_HIGH
-		elif go_up_low:
+		elif jump_low:
 			motion.y = JUMP_HEIGHT_LOW
 		if apply_friction:
 			motion.x = lerp(motion.x, 0, 0.2)
@@ -53,5 +53,5 @@ func _physics_process(delta):
 			$AnimatedSprite.play("Fall")
 		if apply_friction:
 			motion.x = lerp(motion.x, 0, 0.05)
-	
+
 	motion = move_and_slide(motion, UP)
