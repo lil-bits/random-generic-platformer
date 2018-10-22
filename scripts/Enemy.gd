c6a494604103dd584d1011403b367812d6a96d8f
Ondrej Slinták <ondrowan@gmail.com>
2018-10-22 14:28:38 +0200

    Cleanup default enemy scene
    It contained a redundant Area2D with extra CollisionShape2D. Apparently
    Area2D was used for resolving collisions, but it seemed overcomplicated
    to me. I've removed it and resolve them using KinematicBody2D's methods.

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -5,10 +5,11 @@ const UP = Vector2(0.0, -1.0)
 var motion = Vector2(200, 0)
 
 func _physics_process(delta):
-    for body in $Area2D.get_overlapping_bodies():
-        if body.name == "Player":
+    for i in range(self.get_slide_count()):
+        if self.get_slide_collision(i).collider.name == "Player":
             get_tree().change_scene("scenes/StartMenu.tscn")
 
     if is_on_wall():
         motion.x = -motion.x
+
     move_and_slide(motion, UP)
