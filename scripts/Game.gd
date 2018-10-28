f0bf1a30775712bf25f7c2c9a49944e628da40c3
Ondrej Slinták <ondrowan@gmail.com>
2018-10-28 13:47:19 +0100

    Introduce Game singleton
    For now it contains only list of all levels, with their relevant scenes,
    to-be-entrance nodes in world and names. It's better to have this kind
    of information stored globally since it's useful for more scenes. Eg.
    level name is shown at the beginning of given level and also will be
    show in world HUD when player steps on its entrance. When Level holds its
    name it'd be really cumbersome to load the whole level when player steps
    on its entrance in main world just to get that name.

---
+++ scripts/Game.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var levels = {
+    "1-1": {
+        "scene": "res://scenes/Level1.tscn",
+        "entrance_node": "/root/World/Levels/1-1",
+        "name": "First level",
+    },
+    "1-2": {
+        "scene": "res://scenes/Level2.tscn",
+        "entrance_node": "/root/World/Levels/1-2",
+        "name": "Second level",
+    }
+}
--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,12 +1,13 @@
 extends Node
 
-export(String) var level_name
+export(String) var id
 
 var coins = 0
 
 func _ready():
     assert(has_node("Player"))
     assert(has_node("HUD"))
+    assert(Game.levels.has(id))
 
     for enemy in get_tree().get_nodes_in_group("enemies"):
         enemy.connect("enemy_touched", self, "_on_enemy_touched")
@@ -17,7 +18,7 @@ func _ready():
     for coin in get_tree().get_nodes_in_group("coins"):
         coin.connect("coin_taken", self, "_increment_coins")
 
-    $HUD.show_start_level_message(level_name)
+    $HUD.show_start_level_message(Game.levels[id].name)
 
 func _on_trap_triggered():
     get_tree().change_scene("scenes/StartMenu.tscn")
