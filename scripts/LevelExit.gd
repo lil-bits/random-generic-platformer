712c586b26d6e13f5a9d11c4c2e5522149fcb541
Ondrej Slinták <ondrowan@gmail.com>
2018-10-30 15:27:29 +0100

    Use signal for triggering exit from level

--- scripts/LevelExit.gd
+++ scripts/LevelExit.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-func _physics_process(delta):
-    for body in get_overlapping_bodies():
-        if body.name == "Player":
-            get_tree().change_scene("res://scenes/world/World.tscn")
+func _ready():
+    self.connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body):
+    if body.name == "Player":
+        get_tree().change_scene("res://scenes/world/World.tscn")
