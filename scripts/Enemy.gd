ab3d45ae793e33bf15bbd1746b0bef57c41893fa
yanchith <yanchi.toth@gmail.com>
2018-11-04 17:03:06 +1000

    Turn enemy around when end of platform reached by using RayCast2D
    This is implemented by positioning a raycast on the enemy's right side
    and checking whether it collides with ground. Once it stops colliding,
    we flip the enemy's direction and the raycast's position.
    
    This also aplies gravity on enemies so they fall similarly to the way
    players do.

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,18 +3,25 @@ extends KinematicBody2D
 signal enemy_touched
 
 const UP = Vector2(0.0, -1.0)
+const GRAVITY = 20
+const SPEED = 200
 
-var motion = Vector2(200, 0)
+var _direction = 1
+var _motion = Vector2()
 
 func _ready():
     add_to_group("enemies")
 
 func _physics_process(delta):
+    if is_on_wall() or not $RayCast2D.is_colliding():
+        _direction *= -1
+        $RayCast2D.position.x *= -1
+
+    _motion.y += GRAVITY
+    _motion.x = SPEED * _direction
+
     for i in range(self.get_slide_count()):
         if self.get_slide_collision(i).collider.name == "Player":
             emit_signal("enemy_touched")
 
-    if is_on_wall():
-        motion.x = -motion.x
-
-    move_and_slide(motion, UP)
+    _motion = move_and_slide(_motion, UP)
